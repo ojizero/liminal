d0acd5ef9415f1d2a4f68ed473cc294926f11f1e
ojizero <hello@ojizero.tech>
2026-04-24 01:29:12 +0300

    chore: move sqlite in dev/test to db folder

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :liminal, Liminal.Repo,
-  database: Path.expand("../liminal_dev.db", __DIR__),
+  database: Path.expand("../db/liminal_dev.db", __DIR__),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :liminal, Liminal.Repo,
-  database: Path.expand("../liminal_test.db", __DIR__),
+  database: Path.expand("../db/liminal_test.db", __DIR__),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
